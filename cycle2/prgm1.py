6c83381f410d31d8772a1aba835fdcb3282fceb2
Steffi Antony <144442545+Steffi7182@users.noreply.github.com>
2024-10-21 12:20:25 +0530

    Add files via upload

--- cycle2/prgm1.py
+++ cycle2/prgm1.py
@@ -1,7 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
 print("Name:Steffi Antony")
 print("Reg no:SJC23MCA-2054")
-import numpy as np
-array_3d=np.array([ [[1.5,2.6,1.9,4.7],[5.1,6.2,7.2,8.4],[1.8,7.0,4.3,1.0]],
-                  [[19.1,14.5,2.7,8.5],[3.2,1.6,3.9,5.7],[2.7,6.8,9.1,7.0]] ],dtype=float)
-print("3D Array")
-print(array_3d)
+def getdata(url):
+    r = requests.get(url)
+    return r.content
+htmldata = getdata("https://www.toppr.com/guides/essays/globalization-essay/")
+soup = BeautifulSoup(htmldata,'html.parser')
+data = ''
+pr = len(soup.find_all('p'))
+print("<P> tag ",pr)
+for data in soup.find_all('p'):
+    print(data.get_text())
